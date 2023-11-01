6fc425a545b3f845781895262e8217e4d8ec91db
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-01 19:26:42 +0100

    examinamos cada columna por separado

--- lectcsv.py
+++ lectcsv.py
@@ -10,7 +10,8 @@ print('\n Columnas del csv')
 print(datos.columns)
 '''
 Observacón: 
-Hay 3 columnas vacias (precio, dias-excluidos y descripción).
+Hay 5 columnas vacias : precio, dias-excluidos, descripción, unnamed: 29 y audiencia. 
+La audiencia solo tiene un dato por eso la podemos considerar una columna vacia.
 Columnas numéricas son: id evento, gratuito, larga-duración, num-instalación, código postal-instalación, latitud, longitud, coord-x, coord-y)
 El resto son no numéricas. 
 '''
@@ -20,5 +21,14 @@ El resto son no numéricas.
 datos_copia = datos.copy() #realizamos primero una copia de los datos
 
 #Eliminamos las columnas vacias
-datos = datos.drop(['precio', 'dias-excluidos', 'descripción'], axis=1)
+datos = datos.drop(['PRECIO', 'DIAS-EXCLUIDOS', 'DESCRIPCION', 'Unnamed: 29', 'AUDIENCIA'], axis=1)
 print(datos.info())
+
+#Examinamos cada columna por separado
+print(datos.head())
+'''
+Al observar cada columna nos damos cuenta que no podemos completar los espacios en blanco con la media o la mediana. 
+Por lo tanto, eliminaremos las filas que contengan espacios en blanco.
+'''
+#Interpretaremos los 0, 1 de las columnas gratuito y larga-duración como False y True respectivamente
+
