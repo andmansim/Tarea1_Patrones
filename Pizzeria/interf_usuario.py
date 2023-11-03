dfb5e705ddcd977b5bd479c98dfcc3ddfc6ef1d0
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-03 12:27:09 +0100

    lógica cliente

---
+++ Pizzeria/interf_usuario.py
@@ -0,0 +1,9 @@
+'''
+Tiene que meterse el cliente, ya sea con su clave o su nombre. 
+Si es la primera vez que entra, se le pregunta que tipo de pizza quiere y se le guarda en su historial.
+Si ya ha entrado antes, se le pregunta si quiere la misma pizza que la última vez o quiere otra.
+Si quiere la misma, se le pregunta si quiere algún extra o algo diferente.
+Si quiere otra, se le pregunta que tipo de pizza quiere y se le guarda en su historial.
+
+Tiene que haber medidas de seguridad para que no se pueda acceder a los datos de otros clientes.
+'''
